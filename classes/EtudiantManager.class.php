d87b8a4cc4940bf484ec01309f5626a60f3db259
Maxime <maxime.constans@gmail.com>
2021-02-14 00:09:07 +0100

    projet terminé

---
+++ classes/EtudiantManager.class.php
@@ -0,0 +1,59 @@
+<?php
+class EtudiantManager{
+	//A COMPLETER
+    private $db;
+
+    public function __construct($db){
+        $this->db = $db;
+    }
+
+    public function addEtudiant($person){
+        $req=$this->db->prepare
+        ('insert into etudiant(per_num, dep_num, div_num) 
+            values (:per_num, :dep_num, :div_num)');
+
+        $req->bindvalue (':per_num', $person->getPerNum());
+        $req->bindvalue (':dep_num', $person->getDepNum());
+        $req->bindvalue (':div_num', $person->getDivNum());
+
+        $req -> execute ();
+    }
+    public function getEtuByNum($per_num){
+        $etudiant = array();
+
+        $sql = "SELECT * FROM etudiant WHERE per_num = $per_num";
+
+        $requete = $this->db->prepare($sql);
+        $requete->execute();
+
+        while ($pers = $requete->fetch())
+            $etudiant = new Etudiant($pers);
+
+        $requete->closeCursor();
+        return $etudiant;
+    }
+
+    public function getEtudiant($id){
+        $req = $this->db->query(
+            "SELECT per_num, dep_num, div_num FROM etudiant WHERE per_num = $id"
+        );
+        $etudiant = $req->fetch(PDO::FETCH_OBJ);
+        $lEtudiant = new Etudiant($etudiant);
+        $req->closeCursor();
+        return $lEtudiant;
+    }
+
+    public function supEtudiant($etudiant) {
+        $num_etu_del = $etudiant;
+
+        $sql = 'DELETE FROM etudiant WHERE per_num = :per_num';
+        $requete = $this->db->prepare($sql);
+
+        $requete->bindValue(':per_num', $num_etu_del);
+
+        $requete->execute();
+        $requete->closeCursor();
+
+        return $num_etu_del;
+    }
+}
